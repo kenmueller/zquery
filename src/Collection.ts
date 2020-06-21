cfe7b5dd1c83e110d2afb8adf21eeccd862a8226
Ken Mueller <kenmueller0@gmail.com>
2020-06-20 17:56:01 -0700

    create collection table

--- src/Collection.ts
+++ src/Collection.ts
@@ -1,5 +1,38 @@
 import ZQuery from '.'
+import { createHash } from './utils'
 
 export default class Collection {
-	constructor(public parent: ZQuery, public path: string) {}
+	/** The cache of the hash */
+	_hash?: string
+	
+	didCreateTable: boolean = false
+	
+	id: string
+	pathParts: string[]
+	
+	constructor(public parent: ZQuery, public path: string) {
+		this.pathParts = path.split('/')
+		
+		if (!(this.pathParts.length & 1))
+			throw new Error('Collection paths must have an odd number of parts')
+		
+		this.id = this.pathParts[this.pathParts.length - 1]
+	}
+	
+	get hash() {
+		return this._hash ?? (
+			this._hash = createHash(this.path)
+		)
+	}
+	
+	createTableIfNeeded = () =>
+		this.didCreateTable
+			? Promise.resolve() // Don't do anything if you've already created the table
+			: new Promise<void>((resolve, reject) =>
+				this.parent.db.run(
+					// Each document has a random TEXT id
+					`CREATE TABLE IF NOT EXISTS ${this.hash} (id TEXT PRIMARY KEY)`,
+					error => error ? reject(error) : resolve()
+				)
+			)
 }
--- src/Document.ts
+++ src/Document.ts
@@ -1,5 +1,26 @@
 import ZQuery from '.'
+import Collection from './Collection'
 
 export default class Document {
-	constructor(public parent: ZQuery, public path: string) {}
+	id: string
+	pathParts: string[]
+	parentCollection: Collection
+	
+	constructor(public parent: ZQuery, public path: string) {
+		this.pathParts = path.split('/')
+		
+		if (this.pathParts.length & 1)
+			throw new Error('Document paths must have an even number of parts')
+		
+		this.id = this.pathParts[this.pathParts.length - 1]
+		this.parentCollection = this.parent.collection(
+			this.pathParts.slice(0, -1).join('/')
+		)
+	}
+	
+	data = async () => {
+		await this.parentCollection.createTableIfNeeded()
+		
+		// TODO: Select this document from the parent collection's table
+	}
 }
